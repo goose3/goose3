f894c6e8952872fe6cbdee731278608cfed7121b
Tyler Barrus <barrust@gmail.com>
2017-11-11 11:13:01 -0500

    fix inability to properly clean up temp files

--- goose3/__init__.py
+++ goose3/__init__.py
@@ -21,12 +21,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import os
+import shutil
 from tempfile import mkstemp
+import weakref
 
 from goose3.configuration import Configuration
-from goose3.crawler import CrawlCandidate
-from goose3.crawler import Crawler
-# from goose.version import version_info, __version__
+from goose3.crawler import (CrawlCandidate, Crawler)
 
 
 class Goose(object):
@@ -47,18 +47,22 @@ class Goose(object):
             for k, v in list(config.items()):
                 if hasattr(self.config, k):
                     setattr(self.config, k, v)
+
+        # setup a method to clean up everything even if the user fails to do so
+        self._finalizer = weakref.finalize(self, self.close)
+
         # we don't need to go further if image extractor or local_storage is not set
         if not self.config.local_storage_path or not self.config.enable_image_fetching:
             return
+
         # test if config.local_storage_path is a directory
         if not os.path.isdir(self.config.local_storage_path):
             os.makedirs(self.config.local_storage_path)
 
         if not os.path.isdir(self.config.local_storage_path):
-            raise Exception(self.config.local_storage_path +
-                            " directory does not seem to exist, "
-                            "you need to set this for image processing downloads"
-                            )
+            msg = ('{} directory does not seem to exist, you need to set this '
+                   'for image processing downloads').format(self.config.local_storage_path)
+            raise OSError(msg)
 
         # test to write a dummy file to the directory to check is directory is writable
         level, path = mkstemp(dir=self.config.local_storage_path)
@@ -66,14 +70,25 @@ class Goose(object):
             with os.fdopen(level, "w"):
                 pass
             os.remove(path)
-        except IOError:
-            raise Exception(self.config.local_storage_path +
-                            " directory is not writeble, "
-                            "you need to set this for image processing downloads"
-                            )
+        except OSError:
+            msg = ('{} directory is not writeble, you need to set this for '
+                   'image processing downloads').format(self.config.local_storage_path)
+            raise OSError(msg)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def close(self):
+        ''' call all closeout functions '''
+        self.remove_temp_files()
+        # NOTE: storing the requests session at a the Goose level would allow for it to be closed here
+        self.shutdown_network()  # currently does nothing but was already here...
 
     def extract(self, url=None, raw_html=None):
-        """\
+        """
         Main method to extract an article object from a URL,
         pass in a url and get back a Article
         """
@@ -82,6 +97,20 @@ class Goose(object):
 
     def shutdown_network(self):
         pass
+
+    def remove_temp_files(self):
+        ''' Remove all the temporary files; once called will turn of being able to
+            store more temporary files
+        '''
+        tmp_files_list = list(self.config._temporary_files)
+        for filepath in tmp_files_list:
+            try:
+                os.remove(filepath)
+            except:
+                # TODO: log this exception somewhere?
+                print('file {} threw an exception!'.format(filepath))
+                pass
+            self.config._temporary_files.remove(filepath)
 
     def crawl(self, crawl_candidate):
         parsers = list(self.config.available_parsers)
