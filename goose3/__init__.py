769a69e166b5a777351035f9fbabbfef7118eb8b
Tyler Barrus <barrust@gmail.com>
2018-02-18 22:13:07 -0500

    fix infinate parser recursion

--- goose3/__init__.py
+++ goose3/__init__.py
@@ -121,15 +121,20 @@ class Goose(object):
         self.fetcher = None
 
     def __crawl(self, crawl_candidate):
+        ''' wrap the crawling functionality '''
+        def crawler_wrapper(parser, parsers_lst, crawl_candidate):
+            try:
+                crawler = Crawler(self.config, self.fetcher)
+                article = crawler.crawl(crawl_candidate)
+            except (UnicodeDecodeError, ValueError) as ex:
+                if parsers_lst:
+                    parser = parsers_lst.pop(0)  # remove it also!
+                    return crawler_wrapper(parser, pasers_lst, crawl_candidate)
+                else:
+                    raise ex
+            return article
+
+        ''' use the wrapper '''
         parsers = list(self.config.available_parsers)
         parsers.remove(self.config.parser_class)
-        try:
-            crawler = Crawler(self.config, self.fetcher)
-            article = crawler.crawl(crawl_candidate)
-        except (UnicodeDecodeError, ValueError) as ex:
-            if parsers:
-                self.config.parser_class = parsers[0]
-                return self.__crawl(crawl_candidate)
-            else:
-                raise ex
-        return article
+        return crawler_wrapper(self.config.parser_class, parsers, crawl_candidate)
