c61baf0fbc8252a76152fe33c4145c58f29269eb
Tyler Barrus <barrust@gmail.com>
2018-08-05 22:16:35 -0400

    update theme name to resolve conflict

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'custom_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
