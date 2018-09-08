0fd19a42b0a47587be50a1d06e09a55c6813a6bc
Aayush Yadav <aayushyadav96@gmail.com>
2018-09-08 12:57:21 +0530

    remove whitespace

--- goose3/extractors/opengraph.py
+++ goose3/extractors/opengraph.py
@@ -40,7 +40,6 @@ class OpenGraphExtractor(BaseExtractor):
             for meta in metas
             if self.parser.getAttribute(meta, 'property') == "og:type"
         ]
-        
         if og_types:
             og_type = og_types[0] + ":" if og_types[0] else None
 
