2002f016575163fe9a1af9606295ea55224240ca
Immanuel George <immanuel.george@vertical-knowledge.com>
2018-10-25 14:42:25 -0400

    setup for vklab pypi

--- goose3_main_Debug.py
+++ goose3_main_Debug.py
@@ -1,11 +1,5 @@
 from goose3 import Goose
 import requests
-from copy import deepcopy
-
-import lxml.html
-from lxml import etree
-
-from goose3.text import innerTrim, encodeValue, get_encodings_from_content, smart_str
 
 
 def goose_run():
@@ -14,10 +8,8 @@ def goose_run():
     # url = "https://www.facebook.com/135125923270755/posts/friendsi-would-like-you-to-consider-a-proposal-for-an-anti-drone-action-please-p/140479216068759/"
     rqs = requests.get(url)
     g = Goose()
-    import pdb; pdb.set_trace()
     article = g.extract(raw_html=rqs.content)
-    import pdb; pdb.set_trace()
-
+    print(article.title)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,84 +1,32 @@
-# -*- coding: utf-8 -*-
-"""\
-This is a python port of "Goose" orignialy licensed to Gravity.com
-under one or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.
-
-Python port was written by Xavier Grangier for Recrutae
-
-Gravity.com licenses this file
-to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
-import os
-from imp import load_source
-
-from setuptools import (setup, find_packages)
-
-
-def read_file(filepath):
-    ''' read the file '''
-    with open(filepath, 'r') as filepointer:
-        res = filepointer.read()
-    return res
-
-version = load_source("version", os.path.join("goose3", "version.py"))
-
-CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Other Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: POSIX',
-    'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Internet',
-    'Topic :: Utilities',
-    'Topic :: Software Development :: Libraries :: Python Modules']
-
-description = "Html Content / Article Extractor, web scrapping for Python3"
-dependencies = read_file('./requirements/python').splitlines()
-test_dependencies = read_file('./requirements/python-dev').splitlines()
-
-# read long description
-try:
-    long_description = read_file('README.rst')
-except Exception:
-    long_description = description
+from setuptools import setup, find_packages
 
 setup(
-    name='goose3',
-    version=version.__version__,
-    description=description,
-    long_description=long_description,
-    keywords='scrapping, extractor, web scrapping',
-    classifiers=CLASSIFIERS,
-    maintainer='Mahmoud Lababidi',
-    maintainer_email='lababidi+py@gmail.com',
-    url='https://github.com/goose3/goose3',
-    license='Apache',
+    name='vklabs.goose3',
+    version='0.0.1',
+    python_requires='>=3.6',
     packages=find_packages(exclude=['tests']),
-    package_data={'goose3': ['resources/images/*.txt', 'resources/text/*.txt',
-                             'requirements/python']},
     include_package_data=True,
-    zip_safe=False,
-    install_requires=dependencies,
-    test_requires=test_dependencies,
-    test_suite="tests"
+    install_requires=[
+        'beautifulsoup4>=4.6.3',
+        'certifi>=2018.10.15',
+        'chardet>=3.0.4',
+        'cssselect>=1.0.3',
+        'idna>=2.7',
+        'jieba>=0.39',
+        'lxml>=4.2.5',
+        'nltk>=3.3',
+        'Pillow>=5.3.0',
+        'python-dateutil>=2.7.3',
+        'requests>=2.20.0',
+        'six>=1.11.0',
+        'urllib3>=1.24',
+        'flake8>=3.6.0',
+        'requests-mock>=1.5.2'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest>=3.3.0',
+    ],
+    entry_points='''
+    ''',
 )
