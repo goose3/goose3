846532340801e7e32878e00a08549a20b713440f
Immanuel George <immanuel.george@vertical-knowledge.com>
2018-11-01 12:58:22 -0400

    bmp version

--- goose3_main_Debug.py
+++ goose3_main_Debug.py
@@ -6,7 +6,7 @@ def goose_run():
     # url = "https://portal.edirepository.org/nis/metadataviewer?packageid=edi.8.4&contentType=application/xml"
     # url = "https://www.pythonforbeginners.com/requests/using-requests-in-python"
     # url = "https://www.facebook.com/135125923270755/posts/friendsi-would-like-you-to-consider-a-proposal-for-an-anti-drone-action-please-p/140479216068759/"
-    # url = "https://arxiv.org/abs/1808.10784"
+    # url = "http://healthcarenews24.com/13406/global-racing-drone-market-2018-dji-hubsan-parrot-3d-robotics-skytech-yuneec-eachine-immersionrc-lumenier/"
     url = "https://en.wikipedia.org/wiki/Haiti"
     rqs = requests.get(url)
     g = Goose()
