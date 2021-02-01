a80f4c12c396230c15ace8fe70b7cb2d43ca22e8
Tyler Barrus <barrust@gmail.com>
2021-01-31 21:35:47 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -45,9 +45,11 @@ CLASSIFIERS = [
     'Operating System :: Microsoft :: Windows',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Topic :: Internet',
     'Topic :: Utilities',
     'Topic :: Software Development :: Libraries :: Python Modules']
