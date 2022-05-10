525b94c3e14ae1c42044c65976b1c55564cd904d
Sanghoon Kim <galaxytemple@growdle.com>
2022-05-10 15:31:00 +0900

    add comments on TestStopWords

--- tests/test_stopwords.py
+++ tests/test_stopwords.py
@@ -20,10 +20,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import os
 import unittest
 
-from goose3.text import WordStats
 from goose3.text import StopWords
 from goose3.text import StopWordsKorean
 
@@ -34,11 +32,21 @@ class TestStopWordsBase(unittest.TestCase):
         self.lang = 'en'
 
     def test_stop_words(self):
-        text = 'TextNode 1 - The Scala supported IDE is one of the few pain points of developers who want to start using Scala in their Java project.'
-        text += ' On existing long term project developed by a team its hard to step in and introduce a new language that is not supported by the existing IDE.'
-        text += ' On way to go about it is to hid the fact that you use Scala from the Java world by using one way dependency injection.'
-        text += ' Still, if you wish to truly absorb Scala into your existing java environment then you\'ll soon introduced cross language dependencies.'
-        expected = ['the', 'is', 'one', 'of', 'the', 'few', 'of', 'who', 'want', 'to', 'using', 'in', 'their', 'on', 'by', 'its', 'to', 'in', 'and', 'new', 'that', 'is', 'not', 'by', 'the', 'on', 'way', 'to', 'go', 'about', 'it', 'is', 'to', 'the', 'that', 'you', 'use', 'from', 'the', 'by', 'using', 'one', 'way', 'still', 'if', 'you', 'wish', 'to', 'truly', 'into', 'your', 'then', 'soon']
+        # test that all the stopwords present in the paragraph are detected, and also
+        # show that founds are valid
+
+        text = 'TextNode 1 - The Scala supported IDE is one of the few pain points of developers'
+        text += ' who want to start using Scala in their Java project.'
+        text += ' On existing long term project developed by a team its hard to step in and introduce a new language'
+        text += ' that is not supported by the existing IDE.'
+        text += ' On way to go about it is to hid the fact'
+        text += ' that you use Scala from the Java world by using one way dependency injection.'
+        text += ' Still, if you wish to truly absorb Scala into your existing java environment'
+        text += ' then you\'ll soon introduced cross language dependencies.'
+        expected = ['the', 'is', 'one', 'of', 'the', 'few', 'of', 'who', 'want', 'to', 'using', 'in', 'their',\
+                    'on', 'by', 'its', 'to', 'in', 'and', 'new', 'that', 'is', 'not', 'by', 'the', 'on', 'way', 'to', \
+                    'go', 'about', 'it', 'is', 'to', 'the', 'that', 'you', 'use', 'from', 'the', 'by', 'using', 'one', \
+                    'way', 'still', 'if', 'you', 'wish', 'to', 'truly', 'into', 'your', 'then', 'soon']
 
         word_stats = self.stopwords_class(self.lang).get_stopword_count(text)
         
@@ -47,6 +55,7 @@ class TestStopWordsBase(unittest.TestCase):
         self.assertCountEqual(word_stats.get_stop_words(), expected)
         self.assertListEqual(word_stats.get_stop_words(), expected)
 
+        # check that founds are valid
         valid_stop_words = self.stopwords_class(self.lang)._stop_words
         for found_stop_word in word_stats.get_stop_words():
             self.assertIn(found_stop_word, valid_stop_words)
@@ -59,9 +68,11 @@ class TestStopWordsKorean(TestStopWordsBase):
         self.lang = 'ko'
 
     def test_stop_words(self):
-        text = '스칼라는 마틴 오더스키가 자바 제네릭 컴파일러를 개발하며 느꼈던 자바의 여러 가지 단점들을 근본적으로 수정하고, 추후 프로그램 언어 연구를 위한 연구 플랫폼으로 함께 사용하기 위하여 디자인한 언어이다.'
+        text = '스칼라는 마틴 오더스키가 자바 제네릭 컴파일러를 개발하며 느꼈던 자바의 여러 가지 단점들을 근본적으로 수정하고,'
+        text += ' 추후 프로그램 언어 연구를 위한 연구 플랫폼으로 함께 사용하기 위하여 디자인한 언어이다.'
         text += ' 따라서 언뜻 보기에는 자바와 비슷해 보일지 모르나 여러가지 측면에서 더욱 발전된 형태를 가지고 있다.'
-        expected = ['라', '는', '가', '를', '며', '의', '여', '가', '을', '으로', '로', '하고', '고', '로', '를', '으로', '로', '께', '여', '이', '이다', '다', '라', '에', '에는', '는', '와', '나', '여', '가', '에', '에서', '를', '가', '고', '다']
+        expected = ['라', '는', '가', '를', '며', '의', '여', '가', '을', '으로', '로', '하고', '고', '로', '를', '으로', '로', '께', \
+                     '여', '이', '이다', '다', '라', '에', '에는', '는', '와', '나', '여', '가', '에', '에서', '를', '가', '고', '다']
 
         word_stats = self.stopwords_class(self.lang).get_stopword_count(text)
         
