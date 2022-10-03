1ca8838b9e2100fdc3329c85ca2a06f5892e35a1
Tyler Barrus <barrust@gmail.com>
2022-10-03 14:24:43 -0400

    Pep 517 support (#147)
    * first pass at pep-517 support
    * fix build verification

--- setup.py
+++ setup.py
@@ -21,70 +21,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
-from imp import load_source
 
 from setuptools import setup
 
 
-def read_file(filepath):
-    ''' read the file '''
-    with open(filepath, 'r') as filepointer:
-        res = filepointer.read()
-    return res
-
-version = load_source("version", os.path.join("goose3", "version.py"))
-
-CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Other Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: POSIX',
-    'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Topic :: Internet',
-    'Topic :: Utilities',
-    'Topic :: Software Development :: Libraries :: Python Modules']
-
-description = "Html Content / Article Extractor, web scrapping for Python3"
-dependencies = read_file('./requirements/python').splitlines()
-test_dependencies = read_file('./requirements/python-dev').splitlines()
-
-# read long description
-try:
-    long_description = read_file('README.rst')
-except Exception:
-    long_description = description
-
-setup(
-    name='goose3',
-    version=version.__version__,
-    description=description,
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    keywords='scrapping, extractor, web scrapping',
-    classifiers=CLASSIFIERS,
-    maintainer='Mahmoud Lababidi',
-    maintainer_email='lababidi+py@gmail.com',
-    url='https://github.com/goose3/goose3',
-    license='Apache',
-    packages=['goose3', 'goose3.extractors', 'goose3.utils', 'goose3.resources.text', 'goose3.resources.images'],
-    package_data={'goose3': ['resources/images/*.txt', 'resources/text/*.txt',
-                             'requirements/python']},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=dependencies,
-    extras_require={
-        'chinese': ['jieba'],
-        'arabic': ['nltk'],
-        'all': ['jieba', 'nltk']
-    },
-    tests_require=test_dependencies,
-    test_suite="tests"
-)
+setup()
