c24267fdf04fa22f036548c1a3331b2ed6adaaf1
Marcin Wątroba <markowanga@gmail.com>
2023-03-06 23:04:20 +0100

    #112 Add top node raw (#162)

--- goose3/article.py
+++ goose3/article.py
@@ -33,6 +33,7 @@ class Article:
         self._canonical_link = ""
         self._domain = ""
         self._top_node = None
+        self._top_node_raw_html = None
         self._top_image = None
         self._tags = []
         self._opengraph = {}
@@ -129,6 +130,14 @@ class Article:
         Note:
             Read only"""
         return self._top_node
+
+    @property
+    def top_node_raw_html(self):
+        """etree: The top html of Element that is a candidate for the main body of the article without cleaning
+
+        Note:
+            Read only"""
+        return self._top_node_raw_html
 
     @property
     def top_image(self):
