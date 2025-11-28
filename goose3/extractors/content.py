b767b16a3d8383ca1bfb0a43d6fabc1319b5d1f9
barrust <barrust@gmail.com>
2025-11-28 08:56:29 -0500

    update black and re-formatt (minor changes)

--- goose3/extractors/content.py
+++ goose3/extractors/content.py
@@ -19,6 +19,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 from copy import deepcopy
 
 from goose3.extractors import BaseExtractor
--- goose3/extractors/videos.py
+++ goose3/extractors/videos.py
@@ -19,6 +19,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 from goose3.extractors import BaseExtractor
 from goose3.video import Video
 
